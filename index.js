38bc20595d07ea060f19e1703a1e9aa883c0a8e1
puneetvashisht@gmail.com <puneetvashsiht@gmail.com>
2020-04-29 15:03:11 +0530

    initial setup

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+// const MongoClient = require('mongodb').MongoClient;
+
+const app = express()
+const port = 3000
+
+// Connection URL
+const url = 'mongodb://localhost:27017/test';
+// Database Name
+
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const SurveySchema = new mongoose.Schema({
+    text: { type: String, default: '' },
+    date: { type: Date, default: Date.now }
+  });
+const Survey = mongoose.model('Survey', SurveySchema);
+
+
+// parse application/json
+app.use(bodyParser.json())
+app.use(cors())
+app.use(express.static('client/dist/client'))
+
+app.get('/survey', (req, res)=>{
+    Survey.find((err, result)=>{
+      console.log(result);
+      res.json(result);
+    })
+})
+
+app.post('/survey', (req, res)=>{
+    let survey = req.body;
+    // users.push(user);
+    const obj = new Survey();
+    obj.text = survey.text;
+    obj.save((error, result)=> {
+        res.status(201).end();
+    })
+
+  //
+});
+
+   
+    
+    // res.json(users)
+
+
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+module.exports = {
+    app
+};
+
+
+ // MongoClient.connect(url, function(err, client) {
+    //     console.log("Connected successfully to server");
+    //     const db = client.db(dbName);
+    //     const collection = db.collection('users');
+    //     collection.insertOne(user, (error, result)=>{
+    //         if(error) throw error;
+    //         console.log(result);
+    //         res.status(201).end();
+    //     });
+    //   });
